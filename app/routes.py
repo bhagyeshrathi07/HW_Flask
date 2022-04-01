7f960f4e5c43e580352e99ddc218016409db6e41
BhagyeshRathi <63847327+bhagyeshrathi07@users.noreply.github.com>
2022-03-31 22:29:16 -0700

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,21 @@
+from flask import flash, redirect, render_template
+from app import myobj
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask import flash, redirect, render_template, request
-from app import myobj
 
 name = 'Lisa'
 city_names = ['Paris', 'London', 'Rome', 'Tahiti']
 
-class Submit(FlaskForm):
-    city = StringField('City Name', validators=[DataRequired()])
+class newForm(FlaskForm):
+    cityName = StringField('City Name', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-@myobj.route("/", methods =["GET", "POST"])
+@myobj.route("/", methods = ["GET", "POST"])
 def home():
-    
-    form = Submit()
-    if form.validate_on_submit():
-        flash(format(form.city.data))
-        return redirect("/")
-    return render_template('home.html', title='home', name=name, city_names=city_names, form=form)
+    form = newForm()
+    if form.validate_on_submit ():
+        flash(format(form.cityName.data))
+        return redirect('/')
+    return render_template('home.html', title = 'Home', name = name, city_names = city_names, form=form) 
+
